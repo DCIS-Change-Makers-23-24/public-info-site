d6b2de277612677762363d3b5da6e9fe58802834
Krithin Jay Pakshootra <krithinjaycode@gmail.com>
2023-10-31 11:19:24 +0000

    [UPT] add transition effects to cards

--- src/components/Card/HomePageCard.jsx
+++ src/components/Card/HomePageCard.jsx
@@ -3,12 +3,14 @@ import styles from './HomePageCard.module.css'
 
 const HomePageCard = () => {
     return (
+        <div className={styles.homePageCardContainer}>
         <div className={styles.homePageCard}>
             <div className={styles.img}></div>
             <div className={styles.content}>
                 <p className={styles.title}>Lorem ipsum</p>
                 <p className={styles.desc}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam sunt nihil unde necessitatibus voluptatum.</p>
             </div>
+        </div>
         </div>
     )
 }
