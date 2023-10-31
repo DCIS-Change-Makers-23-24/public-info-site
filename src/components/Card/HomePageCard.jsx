44194c1b649c98bb403a7fea4beb8aa66356441a
Krithin Jay Pakshootra <krithinjaycode@gmail.com>
2023-10-31 11:00:58 +0000

    [ADD] added src/components/Card/HomePageCard.jsx, HomePageCard.css

---
+++ src/components/Card/HomePageCard.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './HomePageCard.module.css'
+
+const HomePageCard = () => {
+    return (
+        <div className={styles.homePageCard}></div>
+    )
+}
+
+export default HomePageCard;
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 
 import styles from '../css/index.module.css';
+import HomePageCard from '../components/Card/HomePageCard';
 
 
 export default function Home() {
@@ -14,7 +15,15 @@ export default function Home() {
       title={`Home`}
       description="Description will go into a meta tag in <head />">
       <main className={styles.homePage}>
-        <div className={styles.homePage1}></div>
+          <h1>Insert text here</h1>
+          <div className={styles.homePage1}>
+            <div className={styles.cardContainer}>
+              <HomePageCard />
+              <HomePageCard />
+              <HomePageCard />
+              <HomePageCard />
+            </div>
+          </div>
       </main>
     </Layout>
   );
