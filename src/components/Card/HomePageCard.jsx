d4f77db17633fffa97ba36a09de000c13d6b90f5
Krithin Jay Pakshootra <krithinjaycode@gmail.com>
2023-10-31 11:10:07 +0000

    [UPT] updated styling for cards

--- src/components/Card/HomePageCard.jsx
+++ src/components/Card/HomePageCard.jsx
@@ -3,7 +3,13 @@ import styles from './HomePageCard.module.css'
 
 const HomePageCard = () => {
     return (
-        <div className={styles.homePageCard}></div>
+        <div className={styles.homePageCard}>
+            <div className={styles.img}></div>
+            <div className={styles.content}>
+                <p className={styles.title}>Lorem ipsum</p>
+                <p className={styles.desc}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam sunt nihil unde necessitatibus voluptatum.</p>
+            </div>
+        </div>
     )
 }
 
