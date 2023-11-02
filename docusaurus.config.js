8b2f20d531b3ee4a644fea5b831b47e5e2f8cdf8
Krithin Jay Pakshootra <krithinjaycode@gmail.com>
2023-11-02 12:55:53 +0000

    [CHG] changed docusaurus config for footer and navbar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,8 +8,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'NAE Hackathon',
+  tagline: 'Details, sign ups and resources for the Nord Anglia Education Hackathon',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
@@ -66,29 +66,27 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
-        logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
-        },
+        title: 'NAE Hackathon',
         items: [
-          {
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
-            position: 'left',
-            label: 'Tutorial',
-          },
-          {to: '/blog', label: 'Blog', position: 'left'},
-          {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
-            position: 'right',
-          },
+          {to: '/learn', label: 'Learn', position: 'left'},
         ],
       },
       footer: {
         style: 'dark',
         links: [
+          {
+            title: 'Pages',
+            items: [
+              {
+                label: 'Home',
+                to: "/"
+              },
+              {
+                label: 'Learn',
+                to: "/learn"
+              },
+            ],
+          },
           {
             title: 'Docs',
             items: [
@@ -99,37 +97,16 @@ const config = {
             ],
           },
           {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
-          {
             title: 'More',
             items: [
               {
                 label: 'Blog',
                 to: '/blog',
               },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
-              },
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `No copyright yet?`,
       },
       prism: {
         theme: prismThemes.github,
