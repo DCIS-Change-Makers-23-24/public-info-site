7e51e81521215dd1e9124d03856ef0399260d633
Krithin Jay Pakshootra <krithinjaycode@gmail.com>
2023-11-02 21:03:44 +0800

    Revert "[FIX] fixed import issue at src/pages/Learn"
    This reverts commit 6e6314591831191fc19060c3babcf04d8410fe22.

--- src/pages/Learn.js
+++ src/pages/Learn.js
@@ -7,7 +7,7 @@ import Layout from '@theme/Layout';
 import styles from '../css/Learn.module.css';
 import LearnPageCard from '../components/Card/LearnPageCard';
 
- 
+
 export default function Learn() {
   const {siteConfig} = useDocusaurusContext();
   return (
@@ -18,10 +18,10 @@ export default function Learn() {
           <div className={styles.homePage1}>
           <h1 className={styles.header}>Learn popular frameworks for free</h1>
             <div className={styles.cardContainer}>
-              <LearnPageCard desc={"One of the most popular front-end tools, React is a framework built on Javascript by Facebook."} title="React" />
-              <LearnPageCard desc={"Git and Github are tools that allow programmers to host, share, and collaborate on code."} title="Git/SCM" />
-              <LearnPageCard desc={"HTML/CSS involves the basics of creating websites."} title="HTML/CSS" />
-              <LearnPageCard desc={"Python is one of the most popular, multi-purpose languages used for building backends, AI/ML, data visualisation, etc."} title="Python" />
+              <HomePageCard desc={"One of the most popular front-end tools, React is a framework built on Javascript by Facebook."} title="React" />
+              <HomePageCard desc={"Git and Github are tools that allow programmers to host, share, and collaborate on code."} title="Git/SCM" />
+              <HomePageCard desc={"HTML/CSS involves the basics of creating websites."} title="HTML/CSS" />
+              <HomePageCard desc={"Python is one of the most popular, multi-purpose languages used for building backends, AI/ML, data visualisation, etc."} title="Python" />
             </div>
           </div>
       </main>
