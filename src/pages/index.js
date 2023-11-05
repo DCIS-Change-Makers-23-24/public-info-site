b3f9ea644a040920b8932710d4ec1a2904c7da9b
Krithin Jay Pakshootra <krithinjaycode@gmail.com>
2023-11-05 11:34:50 +0000

    [UPT] updated placeholder text to real text, thanks @Ziggywhatwhat/Adrian

--- src/pages/index.js
+++ src/pages/index.js
@@ -42,35 +42,16 @@ export default function Home() {
                   What is the Nord Anglia Education Hackathon?
                 </span>{" "}
                 <br /> <br />
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Odio
-                eu feugiat pretium nibh. Risus quis varius quam quisque. Pretium
-                viverra suspendisse potenti nullam ac tortor vitae. Sit amet
-                nisl purus in mollis nunc sed id semper. Neque aliquam
-                vestibulum morbi blandit cursus risus at ultrices mi. Nunc
-                aliquet bibendum enim facilisis gravida neque convallis.
-                Molestie nunc non blandit massa enim. Fermentum leo vel orci
-                porta non pulvinar neque. Quam pellentesque nec nam aliquam sem.
-                At varius vel pharetra vel turpis nunc eget. Sem integer vitae
-                justo eget magna fermentum.
+                The upcoming hackathon is designed to be an engaging and intellectually stimulating event that will encompass a range of thought-provoking challenges. These challenges will be focused on assessing participants' critical thinking skills, with a particular emphasis on how technology can be harnessed to address pressing global issues and contribute to the betterment of our world. In order to tackle these challenges effectively, participants are encouraged to craft innovative solutions that ideally integrate both hardware and software components. This unique blend of hardware and software solutions will not only test your technical expertise but also your creative problem-solving abilities, ensuring that you are well-prepared to make a positive impact in our ever-evolving digital age.
                 <br /> <br />
                 <span className={styles.title}>
                   Why should you do the NAE Hackathon?
                 </span>{" "}
                 <br /> <br />
                 <ul>
-                  <li>Lorem ipsum dolor sit amet</li>
-                  <ul>
-                    <li>consectetur adipiscing elit</li>
-                    <li>sed do eiusmod tempor</li>
-                  </ul>
-                  <li>Test test test</li>
-                  <ul>
-                    <li>Helicopters!</li>
-                    <li>Julius Caesar</li>
-                  </ul>
-                  <li>Inclusivity</li>
-                  <li>Oppurtunities for stuff</li>
+                  <li>Skill Development</li><ul><li>FutureHack will require problem-solving, critical thinking, and analytical skills</li><li>By participating, you can sharpen your ability to solve complex technical problems, which will prove to be beneficial in various aspects of your life.</li></ul>
+                  <li>Networking</li><ul><li>FutureHack is designed to garner like-minded individuals who share an interest in technology</li><li>This provides an opportunity to connect with fellow tech enthusiasts, potential mentors, and even future collaborators in tech-related projects.</li></ul>
+                  <li>Competitive Edge</li><ul><li>For students and professionals, excelling in FutureHack and other hackathons can be an impressive addition to your resume or portfolio</li><li>It demonstrates your commitment to continuous learning and your proficiency in the field, which can help you stand out to potential employers or educational institutions.</li></ul>
                 </ul>
               </p>
             </div>
