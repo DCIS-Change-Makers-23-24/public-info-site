c724338714439cd58363e0556eb72728ba7e7324
Krithin Jay Pakshootra <krithinjaycode@gmail.com>
2023-11-07 09:52:20 +0000

    [UPT] re-added old files

---
+++ src/pages/index.js
@@ -0,0 +1,135 @@
+import React from "react";
+import clsx from "clsx";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import styles from "../css/index.module.css";
+import HomePageCard from "../components/Card/LearnPageCard";
+import { useHistory } from "@docusaurus/router";
+export default function Home() {
+  const { siteConfig } = useDocusaurusContext();
+  const history = useHistory()
+  return (
+    <Layout
+      title={`Home`}
+      description="Description will go into a meta tag in <head />"
+    >
+      <main className={styles.homePage}>
+        <div className={styles.homePage1}>
+          <div className={styles.homePage1Overlay}></div>
+          <h1 className={styles.homePage1Header}>FutureHack</h1>
+          <p className={styles.homePage1Desc}>
+            Forging connections amidst the ever-shifting IT landscape.
+          </p>
+          <div className={styles.homePage1BottomBar}>
+            <button className={styles.homePage1BottomBarButton} onClick={() => history.push('/docs/support/get-help')}>
+              Get help
+            </button>
+            <button className={styles.homePage1BottomBarButton} onClick={() => history.push('/docs/support/how-to-join')}>
+              How to join
+            </button>
+          </div>
+        </div>
+        <div className={styles.homePage2}>
+          <div className={styles.homePage2ColumnLayout}>
+            <div className={styles.homePage2Column1}>
+              <p>
+                <span className={styles.title}>
+                  What is the Nord Anglia Education Hackathon?
+                </span>{" "}
+                <br /> <br />
+                The upcoming hackathon is designed to be an engaging and intellectually stimulating event that will encompass a range of thought-provoking challenges. These challenges will be focused on assessing participants' critical thinking skills, with a particular emphasis on how technology can be harnessed to address pressing global issues and contribute to the betterment of our world. In order to tackle these challenges effectively, participants are encouraged to craft innovative solutions that ideally integrate both hardware and software components. This unique blend of hardware and software solutions will not only test your technical expertise but also your creative problem-solving abilities, ensuring that you are well-prepared to make a positive impact in our ever-evolving digital age.
+                <br /> <br />
+                <span className={styles.title}>
+                  Why should you do the NAE Hackathon?
+                </span>{" "}
+                <br /> <br />
+                <ul>
+                  <li>Skill Development</li><ul><li>FutureHack will require problem-solving, critical thinking, and analytical skills</li><li>By participating, you can sharpen your ability to solve complex technical problems, which will prove to be beneficial in various aspects of your life.</li></ul>
+                  <li>Networking</li><ul><li>FutureHack is designed to garner like-minded individuals who share an interest in technology</li><li>This provides an opportunity to connect with fellow tech enthusiasts, potential mentors, and even future collaborators in tech-related projects.</li></ul>
+                  <li>Competitive Edge</li><ul><li>For students and professionals, excelling in FutureHack and other hackathons can be an impressive addition to your resume or portfolio</li><li>It demonstrates your commitment to continuous learning and your proficiency in the field, which can help you stand out to potential employers or educational institutions.</li></ul>
+                </ul>
+              </p>
+            </div>
+            <div className={styles.homePage2Column2}>
+              <p>
+                <span className={styles.title}>Details for the Hackathon</span>{" "}
+                <br />
+                <br />
+                <ul>
+                  <li>Date: dd/mm/yyyy</li>
+                  <li>Duration: 2 days and 3 nights</li>
+                  <li>Location: DCIS@Singapore</li>
+                </ul>
+                <br />
+                <span className={styles.title}>
+                  Categories for the Hackathon
+                </span>{" "}
+                <br />
+                <br />
+                <ol>
+                  <li>Hardware</li>
+                  <li>Software</li>
+                  <li>Both hardware and software</li>
+                </ol>
+                <br />
+                <span className={styles.title}>
+                  Question for the Hackathon
+                </span>{" "}
+                <br />
+                <br />
+                Lorem ipsum dolor, sit amet? Consectetur adipiscing elit
+                programa.
+              </p>
+            </div>
+          </div>
+            <h1 className={styles.title}>
+              What is FutureHack?
+            </h1>
+            <p>FutureHack is an engaging and intellectually stimulating event that will encompass a set of 2 thought-provoking challenges. These challenges will be focused on assessing participants' critical thinking skills, with a particular emphasis on how technology can be harnessed to address pressing global issues and contribute to the betterment of our world. In order to tackle these challenges effectively, participants are encouraged to craft innovative solutions that ideally integrate both hardware and software components. This unique blend of hardware and software solutions will not only test your technical expertise but also your creative problem-solving abilities, ensuring that you are well-prepared to make a positive impact in our ever-evolving digital age.</p>
+        </div>
+        <div className={styles.homePage3}>
+            <h1 className={styles.title}>
+                Why should you do the NAE Hackathon?
+            </h1>
+            <ul>
+              <li>Skill Development</li><ul><li>FutureHack will require problem-solving, critical thinking, and analytical skills</li><li>By participating, you can sharpen your ability to solve complex technical problems, which will prove to be beneficial in various aspects of your life.</li></ul>
+              <li>Networking</li><ul><li>FutureHack is designed to garner like-minded individuals who share an interest in technology</li><li>This provides an opportunity to connect with fellow tech enthusiasts, potential mentors, and even future collaborators in tech-related projects.</li></ul>
+              <li>Competitive Edge</li><ul><li>For students and professionals, excelling in FutureHack and other hackathons can be an impressive addition to your resume or portfolio</li><li>It demonstrates your commitment to continuous learning and your proficiency in the field, which can help you stand out to potential employers or educational institutions.</li></ul>
+            </ul>
+        </div>
+        <div className={styles.homePage4}>
+          <p>
+            <span className={styles.title}>Details for the Hackathon</span>{" "}
+            <br />
+            <br />
+            <ul>
+              <li>Date: dd/mm/yyyy</li>
+              <li>Duration: 2 days and 3 nights</li>
+              <li>Location: DCIS@Singapore</li>
+            </ul>
+            <br />
+            <span className={styles.title}>
+              Categories for the Hackathon
+            </span>{" "}
+            <br />
+            <br />
+            <ol>
+              <li>Hardware</li>
+              <li>Software</li>
+              <li>Both hardware and software</li>
+            </ol>
+            <br />
+            <span className={styles.title}>
+              Question for the Hackathon
+            </span>{" "}
+            <br />
+            <br />
+            Lorem ipsum dolor, sit amet? Consectetur adipiscing elit
+            programa.
+          </p>
+        </div>
+      </main>
+    </Layout>
+    );
+}
