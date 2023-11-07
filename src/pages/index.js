bb283607ece07ed35168b69d56c5c5387b11334d
Krithin Jay Pakshootra <krithinjaycode@gmail.com>
2023-11-07 09:54:55 +0000

    [CHG] reformatting css files

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,8 +4,10 @@ import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 import styles from "../css/index.module.css";
+
 import HomePageCard from "../components/Card/LearnPageCard";
 import { useHistory } from "@docusaurus/router";
+
 export default function Home() {
   const { siteConfig } = useDocusaurusContext();
   const history = useHistory()
