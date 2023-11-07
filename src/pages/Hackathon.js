3509b7bf80b2a6bc407ba83bf517d3ca7d10081d
Krithin Jay Pakshootra <krithinjaycode@gmail.com>
2023-11-07 09:49:43 +0000

    [CHG] moved new design into /hackathon

--- src/pages/Hackathon.js
+++ src/pages/Hackathon.js
@@ -5,7 +5,6 @@ import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 
 import styles from "../css/index.module.css";
-import HomePageCard from "../components/Card/LearnPageCard";
 
 import { useHistory } from "@docusaurus/router";
 import '../css/animation.css'
@@ -18,7 +17,7 @@ import {useRef, useLayoutEffect} from 'react'
 gsap.registerPlugin(ScrollTrigger)
 
 
-export default function Home() {
+export default function Hackathon() {
   const { siteConfig } = useDocusaurusContext();
   const history = useHistory()
   const ref = useRef(null)
@@ -29,7 +28,7 @@ export default function Home() {
     const element = ref.current;
 
     const ctx = gsap.context(() => {
-      gsap.fromTo(element.querySelectorAll(".home-page-1-anim"), {
+      gsap.fromTo(element.querySelectorAll(".hackathon-page-1-anim"), {
         y: -20,
         opacity: 0,
       }, {
@@ -49,7 +48,7 @@ export default function Home() {
     const element = ref.current;
 
     const ctx = gsap.context(() => {
-      gsap.fromTo(element.querySelectorAll(".home-page-2-anim"), {
+      gsap.fromTo(element.querySelectorAll(".hackathon-page-2-anim"), {
         x: -20,
         opacity: 0,
       }, {
@@ -58,11 +57,10 @@ export default function Home() {
         duration: 1,
         stagger: 0.6,
         scrollTrigger: {
-          trigger: element.querySelector(".home-page-2"),
+          trigger: element.querySelector(".hackathon-page-2"),
           start: "top 20%",
           end: "bottom bottom",
           toggleActions: "play none none reverse",
-          markers: true
         }
       })
     }, ref)
@@ -77,7 +75,7 @@ export default function Home() {
     const element = ref.current;
 
     const ctx = gsap.context(() => {
-      gsap.fromTo(element.querySelectorAll(".home-page-3-anim"), {
+      gsap.fromTo(element.querySelectorAll(".hackathon-page-3-anim"), {
         x: 20,
         opacity: 0,
       }, {
@@ -86,11 +84,10 @@ export default function Home() {
         duration: 1,
         stagger: 0.6,
         scrollTrigger: {
-          trigger: element.querySelector(".home-page-3"),
+          trigger: element.querySelector(".hackathon-page-3"),
           start: "top 20%",
           end: "bottom bottom",
           toggleActions: "play none none reverse",
-          markers: true
         }
       })
     }, ref)
@@ -100,27 +97,27 @@ export default function Home() {
 
   return (
     <Layout
-      title={`Home`}
+      title={`hackathon`}
       description="Description will go into a meta tag in <head />"
     >
-      <main className={styles.homePage} ref={ref}>
-        <div className={styles.homePage1}>
-          <div className={styles.homePage1TextContainer}>
-            <h1 className={styles.homePage1Header + " home-page-1-anim"}>FutureHack</h1>
-            <p className={styles.homePage1Desc + " home-page-1-anim"}>
+      <main className={styles.hackathonPage} ref={ref}>
+        <div className={styles.hackathonPage1}>
+          <div className={styles.hackathonPage1TextContainer}>
+            <h1 className={styles.hackathonPage1Header + " hackathon-page-1-anim"}>FutureHack</h1>
+            <p className={styles.hackathonPage1Desc + " hackathon-page-1-anim"}>
               Forging connections amidst the ever-shifting IT landscape.
             </p>
-            <div className={styles.buttonRow + " home-page-1-anim"}>
-              <button className={styles.homePage1Button} onClick={() => history.push('/docs/support/get-help')}>
+            <div className={styles.buttonRow + " hackathon-page-1-anim"}>
+              <button className={styles.hackathonPage1Button} onClick={() => history.push('/docs/support/get-help')}>
                 Get help
               </button>
-              <button className={styles.homePage1Button} onClick={() => history.push('/docs/support/how-to-join')}>
+              <button className={styles.hackathonPage1Button} onClick={() => history.push('/docs/support/how-to-join')}>
                 How to join
               </button>
             </div>
           </div>
-          <div className="home-page-animation">
-            <div className="screen home-page-1-anim">
+          <div className="hackathon-page-animation">
+            <div className="screen hackathon-page-1-anim">
               <div className="title-container">
                 <p className="title">index.js</p>
                 <div className="buttons">
@@ -149,23 +146,23 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div className={styles.homePage2 + " home-page-2"}>
-            <h1 className={styles.title + " home-page-2-anim"}>
+        <div className={styles.hackathonPage2 + " hackathon-page-2"}>
+            <h1 className={styles.title + " hackathon-page-2-anim"}>
               What is FutureHack?
             </h1>
-            <p className={"home-page-2-anim"}>FutureHack is an engaging and intellectually stimulating event that will encompass a set of 2 thought-provoking challenges. These challenges will be focused on assessing participants' critical thinking skills, with a particular emphasis on how technology can be harnessed to address pressing global issues and contribute to the betterment of our world. In order to tackle these challenges effectively, participants are encouraged to craft innovative solutions that ideally integrate both hardware and software components. This unique blend of hardware and software solutions will not only test your technical expertise but also your creative problem-solving abilities, ensuring that you are well-prepared to make a positive impact in our ever-evolving digital age.</p>
+            <p className={"hackathon-page-2-anim"}>FutureHack is an engaging and intellectually stimulating event that will encompass a set of 2 thought-provoking challenges. These challenges will be focused on assessing participants' critical thinking skills, with a particular emphasis on how technology can be harnessed to address pressing global issues and contribute to the betterment of our world. In order to tackle these challenges effectively, participants are encouraged to craft innovative solutions that ideally integrate both hardware and software components. This unique blend of hardware and software solutions will not only test your technical expertise but also your creative problem-solving abilities, ensuring that you are well-prepared to make a positive impact in our ever-evolving digital age.</p>
         </div>
-        <div className={styles.homePage3 + " home-page-3"}>
-            <h1 className={styles.title + " home-page-3-anim"}>
+        <div className={styles.hackathonPage3 + " hackathon-page-3"}>
+            <h1 className={styles.title + " hackathon-page-3-anim"}>
                 Why should you participate in FutureHack?
             </h1>
-            <ul className={"home-page-3-anim"}>
+            <ul className={"hackathon-page-3-anim"}>
               <li>Skill Development</li><ul><li>FutureHack will require problem-solving, critical thinking, and analytical skills</li><li>By participating, you can sharpen your ability to solve complex technical problems, which will prove to be beneficial in various aspects of your life.</li></ul>
               <li>Networking</li><ul><li>FutureHack is designed to garner like-minded individuals who share an interest in technology</li><li>This provides an opportunity to connect with fellow tech enthusiasts, potential mentors, and even future collaborators in tech-related projects.</li></ul>
               <li>Competitive Edge</li><ul><li>For students and professionals, excelling in FutureHack and other hackathons can be an impressive addition to your resume or portfolio</li><li>It demonstrates your commitment to continuous learning and your proficiency in the field, which can help you stand out to potential employers or educational institutions.</li></ul>
             </ul>
         </div>
-        <div className={styles.homePage4}>
+        <div className={styles.hackathonPage4}>
           <div className={styles.parentColumn}>
             <h2 className={styles.subtitle}>
               Questions for the Hackathon
